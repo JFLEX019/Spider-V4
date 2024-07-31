3180c19820488f03bcfdc2f78f4ca7b037d54701
Drillkingg <drillking23@outlook.com>
2024-07-31 06:41:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -23,13 +23,13 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['254104301695']
-global.ownMain = '254104301695'
-global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //gausah diganti 
+global.owner = ['263710264014']
+global.ownMain = '263710264014'
+global.NamaOwner = 'YUNGTEE //gausah diganti 
 global.sessionName = 'spidersession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
-global.namabot = '𝐒𝐏𝐈𝐃𝐄𝐑-𝐕4' //ganti aj klo mau
-global.author = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //ganti aj klo mau
+global.namabot = 'LATISHA' //ganti aj klo mau
+global.author = 'YUNGTEE //ganti aj klo mau
 global.packname = '𝚂𝚙𝚒𝚍𝚎𝚛 𝙱𝚘𝚝' //ganti aj klo mau
 global.yt = 'https://chat.whatsapp.com/EPSGKau0IVi7J5lyOJO7Jk' //gausah diganti
 
